84591ad232dbc5b3f5f0473b95ce34313a6b6547
Miriane de Matos <mirianedematos@outlook.com>
2020-05-23 14:37:23 -0300

    Incluindo descrição e comandos trabalhados na aula1

---
+++ aula1.java
@@ -0,0 +1,13 @@
+/*Aula 1 do Curso 'Fundamentos de programação orientada a objetos com Java', da plaaforma DELL LEAD,
+*que abordou a história e definições técnicas da linguagem e capacitou os alunos para a edição,
+*compilação e execução de um programa*/
+
+public class aula1{
+    public static void main (String args[]){
+        System.out.println ("Hello World!\n" + "Curso Java");
+        System.out.println ("Nome do Filme: A História do Lead");
+        System.out.println ("Ano de Lançamento: 2017");
+        System.out.println ("Bem-vindo!");
+        System.out.println ("Seu nome aqui");
+    }
+}
